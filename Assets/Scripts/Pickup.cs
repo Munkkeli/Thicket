35320d821f51e4990c6078a0e69d57a8824d508a
Tuomas Pöyry <tuomas@munkkeli.io>
2017-11-01 15:16:57 +0200

    Created a placeholder key

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -3,14 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pickup : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+  public string name;
+  public Sprite sprite;
 }
