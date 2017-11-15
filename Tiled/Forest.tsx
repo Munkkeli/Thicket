d9e0e079b88d2e1a224067c09afcf1d6ce5c1948
Tuomas Pöyry <tuomas@munkkeli.io>
2017-11-15 11:44:44 +0200

    Unified map layers on Forest & Thicket

--- Tiled/Forest.tsx
+++ Tiled/Forest.tsx
@@ -1,6 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Forest" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
  <image source="gfx/Overworld.png" width="640" height="576"/>
+ <tile id="151">
+  <objectgroup draworder="index">
+   <object id="1" x="8" y="8" width="15" height="15">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="152">
+  <objectgroup draworder="index">
+   <object id="1" x="-7" y="8" width="15" height="15">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="191">
+  <objectgroup draworder="index">
+   <object id="1" x="8" y="-7" width="15" height="15">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="192">
+  <objectgroup draworder="index">
+   <object id="1" x="-7" y="-7" width="15" height="15">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="206">
   <objectgroup draworder="index"/>
  </tile>
@@ -34,9 +62,26 @@
  <tile id="646">
   <objectgroup draworder="index"/>
  </tile>
+ <tile id="680">
+  <objectgroup draworder="index">
+   <object id="1" x="6" y="3" width="4" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="682">
+  <objectgroup draworder="index">
+   <object id="1" x="5" y="3" width="11" height="11"/>
+   <object id="2" x="6" y="14" width="4" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="683">
+  <objectgroup draworder="index">
+   <object id="2" x="6" y="14" width="4" height="2"/>
+   <object id="4" x="0" y="3" width="11" height="11"/>
+  </objectgroup>
+ </tile>
  <tile id="684">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="1.6875" width="16.0625" height="12.375"/>
+   <object id="6" x="0" y="3" width="16" height="11"/>
   </objectgroup>
  </tile>
  <tile id="685">
@@ -45,14 +90,36 @@
  <tile id="686">
   <objectgroup draworder="index"/>
  </tile>
+ <tile id="720">
+  <objectgroup draworder="index">
+   <object id="1" x="6" y="0" width="4" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="722">
+  <objectgroup draworder="index">
+   <object id="3" x="5" y="4" width="11" height="10"/>
+   <object id="4" x="6" y="0" width="4" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="723">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="4" width="11" height="10"/>
+   <object id="3" x="6" y="0" width="4" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="724">
+  <objectgroup draworder="index">
+   <object id="1" x="6" y="0" width="4" height="16"/>
+  </objectgroup>
+ </tile>
  <tile id="760">
   <objectgroup draworder="index">
-   <object id="1" x="4.75" y="1.75" width="11.25" height="12.4375"/>
+   <object id="3" x="5" y="3" width="11" height="11"/>
   </objectgroup>
  </tile>
  <tile id="761">
   <objectgroup draworder="index">
-   <object id="1" x="-0.125" y="1.75" width="11.1875" height="12.3125"/>
+   <object id="2" x="0" y="3" width="11" height="11"/>
   </objectgroup>
  </tile>
 </tileset>
