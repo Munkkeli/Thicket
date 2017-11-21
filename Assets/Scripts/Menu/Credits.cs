7938674e4bf1c4eda0340f3bd4afcb007137c48b
Crisse (Tony) <33824687+crisseronaldo7@users.noreply.github.com>
2017-11-21 11:40:58 +0200

    Main Menu
    Still working on Main Menu. Not ready yet.

---
+++ Assets/Scripts/Menu/Credits.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour {
+
+	public Button buttonBack;
+
+
+	void Start(){
+		buttonBack = GameObject.Find("Back").GetComponent<Button>();
+		buttonBack.onClick.AddListener(() => SceneManager.LoadScene ("Menu"));
+
+	}
+}
---
+++ Assets/Scripts/Menu/Main.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour {
+
+	public Button buttonPlay;
+	public Button buttonCredits;
+	public Button buttonQuit;
+
+	void Start() {
+		buttonPlay = GameObject.Find("Play").GetComponent<Button>();
+		buttonCredits = GameObject.Find("Credits").GetComponent<Button>();
+		buttonQuit = GameObject.Find("Quit").GetComponent<Button>();
+		buttonPlay.onClick.AddListener(() => SceneManager.LoadScene ("Forest"));
+		buttonCredits.onClick.AddListener(() => SceneManager.LoadScene ("Credits"));
+		//buttonQuit.onClick.AddListener(() => Application.Quit);	
+	}
+}
