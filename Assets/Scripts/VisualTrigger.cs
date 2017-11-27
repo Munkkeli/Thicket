878fcb82f492777922ea97f35dc7453f1a4c9efe
Tuomas Pöyry <tuomas@munkkeli.io>
2017-11-27 18:19:14 +0200

    Added unfinished Thicket map to the game for testing 🎮

---
+++ Assets/Scripts/VisualTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.PostProcessing;
+using UnityEngine;
+
+public class VisualTrigger : MonoBehaviour {
+  public PostProcessingProfile profile;
+  public int visibility = 0;
+
+  void OnTriggerEnter2D (Collider2D collider) {
+    if (collider.transform.parent.gameObject == Controller.current.player && Visuals.current.Check(profile)) {
+      Debug.Log("Switch " + profile.name);
+      Visuals.current.Switch(profile, visibility);
+    }
+  }
+}
--- Assets/Scripts/Visuals.cs
+++ Assets/Scripts/Visuals.cs
@@ -13,10 +13,17 @@ public class Visuals : MonoBehaviour {
   public bool hasClouds = true;
   public Color background;
   public PostProcessingProfile profile;
+  public float transitionSpeed = 2f;
 
   public float visibilityUnits { get { return (grid.square) * visibility; } }
 
   private Pathfinding.Grid grid;
+
+  private PostProcessingProfile profileOriginal;
+  private PostProcessingBehaviour normalPost;
+  private PostProcessingProfile profileSecondary;
+  private float activeContribution = 1;
+  private float activeContributionVel;
 
   void Awake() {
     grid = GetComponent<Pathfinding.Grid>();
@@ -24,6 +31,35 @@ public class Visuals : MonoBehaviour {
   }
 
   void Start() {
-    Controller.current.viewport.viewport.GetComponent<PostProcessingBehaviour>().profile = profile;
+    normalPost = Controller.current.viewport.viewport.GetComponent<PostProcessingBehaviour>();
+    profileOriginal = profile;
+    normalPost.profile = Instantiate(profileOriginal);
+  }
+
+  void Update() {
+    activeContribution = Mathf.SmoothDamp(activeContribution, profileSecondary != null ? 0 : profileOriginal.userLut.settings.contribution, ref activeContributionVel, transitionSpeed);
+
+    if (profileSecondary != null && activeContribution < 0.01f) {
+      profileOriginal = profileSecondary;
+      profile = Instantiate(profileOriginal);
+      profileSecondary = null;
+      normalPost.profile = profile;
+    }
+
+    UserLutModel.Settings lut = normalPost.profile.userLut.settings;
+    lut.contribution = activeContribution;
+    normalPost.profile.userLut.settings = lut;
+  }
+
+  public void Switch(PostProcessingProfile profile, int visibility = 0) {
+    profileSecondary = profile;
+    if (visibility > 0) this.visibility = visibility;
+  }
+
+  public bool Check(PostProcessingProfile profile) {
+    if (profileSecondary != null && profileSecondary == profile) return false;
+    if (profileOriginal == profile && profileSecondary != null) return true;
+    if (profileOriginal == profile) return false;
+    return true;
   }
 }
